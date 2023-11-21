38ec4835d281ae4a2acc2a81c3cab77dda2352fb
phucltt <149924877+phucltt@users.noreply.github.com>
2023-11-21 10:57:55 +0700

    Update App.js

--- todo-list/App.js
+++ todo-list/App.js
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
-import { StyleSheet, View, Text, TextInput, Button, FlatList, TouchableOpacity } from 'react-native';
+import { StyleSheet, View, Text, TextInput, Button, FlatList, TouchableOpacity, Picker } from 'react-native';
 const App = () => {
   const [tasks, setTasks] = useState([]);
   const [text, setText] = useState('');
   const [search, setSearch] = useState('');
+  const [selectedValue, setSelectedValue] = useState('Chân tay'); // State cho Picker
   const addTask = () => {
-    setTasks([...tasks, { key: Math.random().toString(), value: text, completed: false }]);
+    setTasks([...tasks, { key: Math.random().toString(), value: text, completed: false, category: selectedValue }]);
     setText('');
   };
   const completeTask = key => {
@@ -15,33 +16,44 @@ const App = () => {
     setTasks(tasks.filter(task => task.key !== key));
   };
   const renderTask = ({ item }) => (
-  <View style={styles.task}>
-    <Text style={item.completed ? styles.taskTextCompleted : styles.taskText}>{item.value}</Text>
-    <Button title={item.completed ? 'Hoàn tác' : 'Hoàn thành'} onPress={() => completeTask(item.key)} color={item.completed ? 'blue' : 'green'} />
-    <Button title="Xóa" onPress={() => deleteTask(item.key)} color="red" />
-  </View>
-);
-  return (
-    <View style={styles.container}>
-      <TextInput 
-        style={styles.input} 
-        placeholder="Nhập vào" 
-        value={text} 
-        onChangeText={setText} 
-      />
-      <Button title="Thêm" onPress={addTask} />
-      <TextInput
-        style={styles.input}
-        placeholder="Tìm kiếm"
-        value={search}
-        onChangeText={setSearch}
-      />
-      <FlatList
-        data={tasks.filter(task => task.value.includes(search))}
-        renderItem={renderTask}
-      />
+    <View style={styles.task}>
+      <Text style={item.completed ? styles.taskTextCompleted : styles.taskText}>{item.value}</Text>
+      <Text style={styles.categoryText}>{item.category}</Text>
+      <Button title={item.completed ? 'Hoàn tác' : 'Đánh dấu hoàn thành'} onPress={() => completeTask(item.key)} color={item.completed ? 'blue' : 'green'} />
+      <Button title="Xóa" onPress={() => deleteTask(item.key)} color="red" />
     </View>
   );
+  return (
+  <View style={styles.container}>
+    <TextInput
+      style={styles.input}
+      placeholder="Nhập tên công việc"
+      value={text}
+      onChangeText={setText}
+    />
+    <Text style={styles.label}>Phân loại công việc</Text> {/* Thêm dòng text này */}
+    <Picker
+      selectedValue={selectedValue}
+      style={styles.picker}
+      onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}
+    >
+      <Picker.Item label="Chân tay" value="Chân tay" />
+      <Picker.Item label="Trí óc" value="Trí óc" />
+      <Picker.Item label="Cả hai" value="Cả hai" />
+    </Picker>
+    <Button title="Thêm công việc" onPress={addTask} />
+    <TextInput
+      style={styles.input}
+      placeholder="Tìm kiếm công việc"
+      value={search}
+      onChangeText={setSearch}
+    />
+    <FlatList
+      data={tasks.filter(task => task.value.includes(search))}
+      renderItem={renderTask}
+    />
+  </View>
+);
 };
 const styles = StyleSheet.create({
   container: {
@@ -52,6 +64,11 @@ const styles = StyleSheet.create({
     marginVertical: 10,
     borderWidth: 1,
     borderColor: '#ccc',
+  },
+  picker: {
+    height: 50,
+    width: '100%',
+    marginVertical: 10,
   },
   task: {
     flexDirection: 'row',
@@ -65,6 +82,14 @@ const styles = StyleSheet.create({
     marginRight: 10,
     textDecorationLine: 'line-through',
     color: 'green',
+  },
+  categoryText: {
+    marginRight: 10,
+    fontWeight: 'bold',
+  },
+  label: {
+    fontWeight: 'bold',
+    marginVertical: 7,
   },
 });
 export default App;
